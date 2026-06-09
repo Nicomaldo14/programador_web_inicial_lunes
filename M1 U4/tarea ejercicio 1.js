63cd947395ebba42319367964f0d375a2acc53b8
Nicolas maldonado <maldonico793@gmail.com>
2026-06-09 20:17:33 -0300

    practica java

--- M1 U4/tarea ejercicio 1.js
+++ M1 U4/tarea ejercicio 1.js
@@ -1,6 +1,9 @@
 const producto = 'pelota de futbol'
+
 const precio = 1000
+
 const disponible = true
+
 console.log(`El producto es ${producto}, su precio es ${precio} 
     y su disponibilidad es ${disponible}.`)
     
