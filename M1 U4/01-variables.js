0173b01447eeaf0dde97b952b5c8388965ac2a16
Nicolas maldonado <maldonico793@gmail.com>
2026-06-09 18:52:26 -0300

    javascript

---
+++ M1 U4/01-variables.js
@@ -0,0 +1,24 @@
+//console.log('hola soy del curso de programador')
+
+const nombre = 'Nico' // tipo texto o string
+const edad = 30 // tipo numero o number
+//console.log(edad) 
+const casado = true // tipo booleano o boolean
+//const se usa para declarar variables que pueden cambiar su valor a lo largo del programa
+
+
+//csole.log(`Mi nombre es ${nombre} y tengo ${edad} años.`) // interpolacion de variables
+
+//let se usa para declarar variables que pueden cambiar su valor a lo largo del programa
+let contador =0
+contador = contador + 1
+//console.log(contador)
+//array
+
+const frutas = ['manzana', 'banana', 'naranja'] // tipo array o arreglo
+const seleccionado = frutas[2] // accediendo al segundo elemento del array 
+//console.log(seleccionado)
+
+const texto = 'No por mucho madrugrar amanece más temprano' // tipo string o texto
+console.log(texto.length) // longitud del texto
+console.log(frutas.length) // longitud del array
