ad8bd467a2fb31a66d7a741f86f2249201f7fd1d
Nicolas maldonado <maldonico793@gmail.com>
2026-06-10 19:02:48 -0300

    tarea

---
+++ M1 U4/08-ejercicio2.js
@@ -0,0 +1,13 @@
+const pie <= 1000
+const bicicleta > 1000 y <= 10000 
+const colectivo > 10000 y <= 30000 
+const auto > 30000 y <= 100000 
+const avion > 100000 
+
+console.log(`Si la distancia es menor o igual a ${pie} metros, se puede ir a pie.`)
+
+//console.log(`Si la distancia es menor o igual a ${pie} metros, se puede ir a pie.`)
+//console.log(`Si la distancia es mayor a ${pie} metros y menor o igual a ${bicicleta} metros, se puede ir en bicicleta.`)
+//console.log(`Si la distancia es mayor a ${bicicleta} metros y menor o igual a ${colectivo} metros, se puede ir en colectivo.`)
+//console.log(`Si la distancia es mayor a ${colectivo} metros y menor o igual a ${auto} metros, se puede ir en auto.`)
+//console.log(`Si la distancia es mayor a ${auto} metros, se puede ir en avión.`)
---
+++ M1 U4/09-ejercicio3.js
@@ -0,0 +1,5 @@
+const compras=[`leche`,`pan`,`huevos`,`azucar`]
+console.log(`No te olvides de comprar ${compras[0]}`)
+console.log(`No te olvides de comprar ${compras[1]}`)
+console.log(`No te olvides de comprar ${compras[2]}`)
+console.log(`No te olvides de comprar ${compras[3]}`)   
