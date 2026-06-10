7c8fdc2893c2f4bbaff49fac5963d4ac950a66cb
Nicolas maldonado <maldonico793@gmail.com>
2026-06-10 19:06:09 -0300

    tarea

--- M1 U4/08-ejercicio2.js
+++ M1 U4/08-ejercicio2.js
@@ -5,7 +5,6 @@ const auto > 30000 y <= 100000
 const avion > 100000 
 
 console.log(`Si la distancia es menor o igual a ${pie} metros, se puede ir a pie.`)
-
 //console.log(`Si la distancia es menor o igual a ${pie} metros, se puede ir a pie.`)
 //console.log(`Si la distancia es mayor a ${pie} metros y menor o igual a ${bicicleta} metros, se puede ir en bicicleta.`)
 //console.log(`Si la distancia es mayor a ${bicicleta} metros y menor o igual a ${colectivo} metros, se puede ir en colectivo.`)
