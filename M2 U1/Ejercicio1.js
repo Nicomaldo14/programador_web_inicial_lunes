2a7616afef5148f4794bdedccb93b21dde7cb245
Nicolas maldonado <maldonico793@gmail.com>
2026-06-16 20:45:02 -0300

    java2tarea

---
+++ M2 U1/Ejercicio1.js
@@ -0,0 +1,9 @@
+function calcularPromedio (n1,n2) 
+{ const promedio= (n1+n2)/2;
+return promedio;}
+
+const nota1 = 8;
+const nota2 = 6;
+const promedioHistoria = calcularPromedio(nota1,nota2);
+
+console.log ("El promedio de la materia Historia es de:",promedioHistoria);
