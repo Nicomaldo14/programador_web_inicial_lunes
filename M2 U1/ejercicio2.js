6c3dec1d9a614b08364312311791ead30aba1b1f
Nicolas maldonado <maldonico793@gmail.com>
2026-06-23 18:25:13 -0300

    ejercicio 2 completado

---
+++ M2 U1/ejercicio2.js
@@ -0,0 +1,9 @@
+const texto = document.getElementById("texto");
+
+const contar = document.getElementById('contar');
+  
+//console.log(texto,contar)
+
+texto.addEventListener('keyup', function(){
+    contar.innerText = texto.value.length
+})
