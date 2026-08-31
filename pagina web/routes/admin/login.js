8a8fc3f8e7968e613d621c225483b3fea8453697
Nicolas maldonado <maldonico793@gmail.com>
2026-08-31 18:26:24 -0300

    login logout

--- pagina web/routes/admin/login.js
+++ pagina web/routes/admin/login.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var usuarioModel = require('../../models/usuarioModel');
+var usuarioModel = require('../../models/usuariosModels');
 
 
 /* GET home page. */
